d9be4f4da64e2051e80023bdfd4fa8b421381cc6
kanishka270307 <sharmakanishka098@gmail.com>
2025-09-15 15:05:30 +0530

    Delete largest of three numbers.py

--- largest of three numbers.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-c = int(input("Enter third number: "))
-if a >= b and a >= c:
-    print(f"{a} is the largest number.")
-elif b >= a and b >= c:
-    print(f"{b} is the largest number.")
-else:
-    print(f"{c} is the largest number.")
